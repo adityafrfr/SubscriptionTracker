59ba3bd029f998669b9e3aa7337bfa9904a0480d
Aditya <mail4adityaa@gmail.com>
2026-06-04 18:49:13 +0530

    some auth logic

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -5,19 +5,19 @@ const errorMiddleware = (err, req, res, next) => {
         error.message = err.message
         console.error(err)
 
-        if (err.name === 'CastError')   {
+        if (err.name === 'CastError') {
             const message = 'Resource not found'
             error = new Error(message)
             error.statusCode = 404
         }
 
-        if (err.code === 11000)   {
+        if (err.code === 11000) {
             const message = 'Duplicate field value entered'
             error = new Error(message)
             error.statusCode = 400
         }
 
-        if (err.name === 'ValidationError')   {
+        if (err.name === 'ValidationError') {
             const message = Object.values(err.errors).map(val => val.message)
             error = new Error(message.join(', '))
             error.statusCode = 400
