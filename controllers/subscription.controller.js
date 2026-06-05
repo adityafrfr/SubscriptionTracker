87fe5839a5bd2c3e1febc9f4ea99f99ce4a688b7
Aditya <mail4adityaa@gmail.com>
2026-06-05 15:02:53 +0530

    Subscription done

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -40,8 +40,118 @@ export const getAllSubscriptions = async (req, res, next) => {
         }
         const subscriptions = await Subscription.find({})
         res.status(200).json({
+            success: true, data: subscriptions
+        })
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getAllRenewals = async (req, res, next) => {
+    try {
+        if (req.user._id.toString() !== req.params.id) {
+            const error = new Error('Unauthorised')
+            error.statusCode = 401
+            throw error
+        }
+
+        const allSubscriptions = await Subscription.find({
+            user: req.params.id, renewalDate: {$gte: new Date()}
+        })
+
+        res.status(200).json({
+            success: true, data: allSubscriptions
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getSubscriptionById = async (req, res, next) => {
+    try {
+        const subscription = await Subscription.findById(req.params.id)
+        if (!subscription) {
+            const error = new Error('No subscription by that id')
+            error.statusCode = 404
+            throw error
+        }
+
+        if (subscription.user.toString() !== req.user._id.toString()) {
+            const error = new Error('unauthorised you are not the owner')
+            error.statusCode = 401
+            throw error
+        }
+
+        res.status(200).json({
+            success: true, data: subscription
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const cancelSubscriptionById = async (req, res, next) => {
+    try {
+        const subscription = await Subscription.findById(req.params.id)
+        if (!subscription) {
+            const error = new Error('not on server')
+            error.statusCode = 404
+            throw error
+        }
+
+        if (subscription.user.toString() !== req.user._id.toString()) {
+            const error = new Error('unauthorised you are not the owner')
+            error.statusCode = 401
+            throw error
+        }
+
+        if (subscription.status === 'cancelled') {
+            const error = new Error('Subscription is already cancelled')
+            error.statusCode = 400
+            throw error
+        }
+
+        const newSubscription = await Subscription.findByIdAndUpdate(req.params.id, {status: 'cancelled'}, {new: true})
+
+        res.status(200).json({
+            success: true, data: newSubscription
+        })
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateSubscriptionById = async (req, res, next) => {
+    try {
+        const subscription = await Subscription.findById(req.params.id)
+        if (!subscription) {
+            const error = new Error('Subscription Doesnt exist')
+            error.statusCode = 404
+            throw error
+        }
+
+        if (req.user.role !== 'admin') {
+            const error = new Error('unauthorised')
+            error.statusCode = 403
+            throw error
+        }
+
+        const {name, price, currency, frequency, category, paymentMethod, status} = req.body
+
+        const newSubscription = await Subscription.findByIdAndUpdate(req.params.id, {
+            name,
+            price,
+            currency,
+            frequency,
+            category,
+            paymentMethod,
+            status
+        }, {new: true, runValidators: true})
+        res.status(200).json({
             success: true,
-            data: subscriptions
+            data: newSubscription
         })
 
     } catch (error) {
--- routes/subscriptions.routes.js
+++ routes/subscriptions.routes.js
@@ -1,6 +1,12 @@
 import {Router} from "express";
 import authorize from "../middleware/auth.middleware.js";
-import {createSubscription, getAllSubscriptions, getUserSubscriptions} from "../controllers/subscription.controller.js";
+import {
+    cancelSubscriptionById,
+    createSubscription,
+    getAllRenewals,
+    getAllSubscriptions, getSubscriptionById,
+    getUserSubscriptions, updateSubscriptionById
+} from "../controllers/subscription.controller.js";
 import authorizeRole from "../middleware/role.middleware.js";
 
 const subscriptionsRouter = Router()
@@ -9,31 +15,14 @@ subscriptionsRouter.get(`/`, authorize, authorizeRole('admin'), getAllSubscripti
 
 subscriptionsRouter.get(`/user/:id`, authorize, getUserSubscriptions)
 
-subscriptionsRouter.get(`/upcoming-renewals`, (req, res) => {
-    // TODO: get upcoming renewals logic
-    res.send({title: 'get all upcoming renewals'})
-})
+subscriptionsRouter.get(`/upcoming-renewals/:id`, authorize, getAllRenewals)
 
 subscriptionsRouter.post(`/user/:id`, authorize, createSubscription)
 
-subscriptionsRouter.get(`/:id`, (req, res) => {
-    // TODO: get single subscription by id logic
-    res.send({title: 'get subscription details'})
-})
+subscriptionsRouter.get(`/:id`, authorize, getSubscriptionById)
 
-subscriptionsRouter.put(`/:id/cancel`, (req, res) => {
-    // TODO: cancel subscription logic
-    res.send({title: 'cancel subscription'})
-})
+subscriptionsRouter.put(`/:id/cancel`, authorize, cancelSubscriptionById)
 
-subscriptionsRouter.put(`/:id`, (req, res) => {
-    // TODO: update subscription logic
-    res.send({title: 'Update a subscription'})
-})
-
-subscriptionsRouter.delete(`/:id`, (req, res) => {
-    // TODO: delete subscription logic
-    res.send({title: 'delete subscription'})
-})
+subscriptionsRouter.put(`/:id`, authorize, authorizeRole('admin'), updateSubscriptionById)
 
 export default subscriptionsRouter
